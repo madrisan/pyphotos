5167685743da9107c0c30da6a2fa6c3a23e36adb
Davide Madrisan <davide.madrisan@gmail.com>
2015-03-17 00:18:29 +0100

    OptionParser is deprecated, move to argparse
    Signed-off-by: Davide Madrisan <davide.madrisan@gmail.com>

--- photo-rename.py
+++ photo-rename.py
@@ -1,27 +1,34 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
-
-from optparse import OptionParser
 from PIL import Image
 from PIL.ExifTags import TAGS
 
-import errno, os, shutil, sys
+import argparse, errno, os, shutil, sys
 
-options = \
-    OptionParser(
-        version = "%prog v1",
-        usage = '%prog /path/to/the/photo',
-        description = 'Simple Photo Renaming Tool',
-        epilog = 'Example: ' + os.path.basename(sys.argv[0])
-               + ' -d ../myphotos DSCN6529.JPG')
-
-options.add_option('-d', '--destdir', default="./", dest="destdir", type="string",
-        action='store', help='destination directory')
-options.add_option('-f', '--force', default=False, dest="force",
-        action='store_true', help='overwrite any existing output file')
-options.add_option('-v', '--verbose', default=False, dest="verbose",
-        action='store_true', help='switch to the verbose mode')
+parser = argparse.ArgumentParser(
+    description = 'Simple photo renaming tool.',
+    epilog = 'example: ' + os.path.basename(sys.argv[0])
+           + ' -d ../myphotos DSCN6529.JPG')
+parser.add_argument(
+    "-d", "--destdir",
+    action = "store",
+    help   = "specify a destination directory",
+    dest   = "destdir")
+parser.add_argument(
+    "-f", "--force",
+    action = "store_true",
+    help   = "overwrite any existing output file",
+    dest   = "force")
+parser.add_argument(
+    "-v", "--verbose",
+    action = "store_true",
+    help   = "execute this script in verbose mode",
+    dest   = "verbose")
+parser.add_argument(
+    "--version", action = "version", version = "%(prog)s version 1")
+parser.add_argument(
+    "imageFile", help = "the image file to be renamed")
 
 def GetExifTags(fname):
     """Get embedded EXIF data from image file."""
@@ -51,28 +58,23 @@ def ForgeNewName(imageFile, destdir):
     return "%s/%s_%s%s" % (destdir, date,time, fileExtension.lower())
 
 def main():
-    (opts, args) = options.parse_args()
-    if len(args) < 1:
-        options.print_help()
-        return
+    args = parser.parse_args()
 
-    imageFileSrc = args[0]
-    imageFileDst = ForgeNewName(imageFileSrc, opts.destdir)
+    imageFileDst = ForgeNewName(args.imageFile, args.destdir)
 
-    if opts.verbose:
-        print(imageFileSrc + ' --> ' + imageFileDst)
+    if args.verbose:
+        print(args.imageFile + ' --> ' + imageFileDst)
 
-    if (os.path.isfile(imageFileDst) and not opts.force):
+    if (os.path.isfile(imageFileDst) and not args.force):
         sys.stderr.write(
             'The destination file %s already exists\n' % imageFileDst)
         sys.exit(1)
 
     try:
-        shutil.move(imageFileSrc, imageFileDst)
+        shutil.move(args.imageFile, imageFileDst)
     except Exception as e:
         sys.stderr.write('An IO error occurred: %s\n' % e)
         sys.exit(1)
 
 if __name__ == '__main__':
     main()
-
